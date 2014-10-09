46e2318875edccc15c0a7093686c4ab1ffe6865e
Paul Rivière <paul@paulriviere.fr>
2014-10-09 22:53:52 +0200

    Ajout diagrammes cas utilisation

---
+++ SmallWorld/FabriqueCase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmallWorld
+{
+    public class FabriqueCase
+    {
+        public CasePlaine CasePlaine
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public CaseDesert CaseDesert
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public CaseMontagne CaseMontagne
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public CaseForet CaseForet
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
