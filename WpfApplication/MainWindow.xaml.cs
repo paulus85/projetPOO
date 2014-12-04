ae1286d7ab9f242f83dfed2bccf1923639cbe59d
paulus85 <paul@paulriviere.fr>
2014-12-04 11:59:03 +0100

    Travail sur xaml -> structure de polygon

--- WpfApplication/MainWindow.xaml.cs
+++ WpfApplication/MainWindow.xaml.cs
@@ -24,10 +24,50 @@ namespace WpfApplication
 
         SmallWorld.CarteImpl carte;
         const int dimensionCase = 5;
+        private string champ;
+        public string Champ
+        {
+            get { return champ; }
+            set
+            {
+                if (champ == value)
+                    return;
+                champ = value;
+                //PropertyChanged("Champ");
+            }
+        }
 
         public MainWindow()
         {
             InitializeComponent();
+            champ = "Default";
+            this.DataContext = this;
+            /*
+            ImageBrush brush = new ImageBrush();
+            brush.ImageSource = new BitmapImage (new Uri("C:/Users/Paul/Documents/SmallWorld/WpfApplication/Ressources/desert.png", UriKind.Absolute));
+            ImageBrush brush2 = new ImageBrush();
+            brush2.ImageSource = new BitmapImage(new Uri("C:/Users/Paul/Documents/SmallWorld/WpfApplication/Ressources/foret.png", UriKind.Absolute));
+            Rectangle rec = new Rectangle();
+            //canvas.Children.Add(new Rectangle())
+            rec.Height = 79;
+            rec.Width = 69;
+            rec.Fill = brush;
+            rec.StrokeThickness = 4;
+            rec.Stroke = Brushes.Black;
+            canvas.Children.Add(rec);
+            Canvas.SetZIndex(rec, 2);
+
+
+            Rectangle rec2 = new Rectangle();
+            rec2.Height = 79;
+            rec2.Width = 69;
+            rec2.Fill = brush2;
+            rec2.Margin = new Thickness(69,0,0,0);
+            canvas.Children.Add(rec2);
+            Canvas.SetZIndex(rec2, 2);
+            */
+
+
         }
 
         public void creerCarte(){
@@ -37,7 +77,23 @@ namespace WpfApplication
             carte.Cases[0,0] = carte.fabrique.creerDesert();
             Rectangle rec = new Rectangle();
             //canvas.Children.Add(new Rectangle())
+            rec.Height = 10;
+            rec.Width = 20;
+            rec.Stroke = Brushes.Aqua;
+            canvas.Children.Add(rec);
         }
+
+        public void creerCase(SmallWorld.Case c, int x, int y) {
+
+            Polygon caseDraw = new Polygon();
+            caseDraw.Points = new PointCollection();
+        }
+
+        //private void Path_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    champ = sender.ToString();
+        //    Console.WriteLine(champ);
+        //}
        
 
         
