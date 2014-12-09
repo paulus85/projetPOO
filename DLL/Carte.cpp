aae2d88a68893b6b44cececb0e2eceb2822ec4a1
Stone <pierre.poilane@insa-rennes.fr>
2014-12-09 12:25:33 +0100

    Modif' DLL

--- DLL/Carte.cpp
+++ DLL/Carte.cpp
@@ -1,11 +1,12 @@
 #include "Carte.h"
 #include "Enum.h"
+#include "Joueur.h"
 #include <stdlib.h>
 #include <time.h>
 
 
 
-Carte::Carte(int nbCases) {
+Carte::Carte(int nbCases, Joueur* joueur1, Joueur* joueur2) {
 	srand((unsigned int)time(NULL));
 	int controleTab[4] = {0, 0, 0, 0};
 	int val;
@@ -21,13 +22,16 @@ Carte::Carte(int nbCases) {
 	//remplissage de la carte
 	for (int i = 0; i < nbCases; i++){
 		for (int j = 0; j < nbCases; j++){
-			/*while (controleTab[val] == 9)
-			{*/
+			while (controleTab[val] == 9)
+			{
 				val = rand() % 4;
 				_carte[i][j] = (Tuile)val;
-			//}
+			}
 		}
 	}
+
+	joueur1->setCoordonnées(0, 0);
+	joueur2->setCoordonnées(nbCases - 1, nbCases - 1);
 }
 
 Tuile** Carte::getCarte(){ 
---
+++ DLL/Enum.h
@@ -0,0 +1,8 @@
+#pragma once
+
+enum class Tuile {
+	Plaine = 0,
+	Désert = 1,
+	Montagne = 2,
+	Forêt = 3
+};
